778812becb507d8882d74657114c8465a2627acf
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-06 05:32:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
+     if params[:username]=="" || params[:password]==""
+      redirect to '/failure'
+    end
+     
      user = User.find_by(:username => params[:username])
 
     if user && user.authenticate(params[:password])
